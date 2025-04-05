c87d919d7fcbb095eaa8f8056ff390390b7ea804
Harshal2826 <mudgal10harshal@gmail.com>
2025-04-05 06:54:48 +0000

    day-1

---
+++ basics01/conversion.js
@@ -0,0 +1,17 @@
+let score=null 
+
+console.log(typeof score)
+
+let valueinString=String(score)
+console.log(typeof valueinString);
+
+
+//"33"=>33
+//converting from string to number "33ab"=>NaN
+//true=>1
+
+//let isLoggedIn=1
+let isLoggedIn="harshal"
+
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
---
+++ basics01/variable.js
@@ -0,0 +1,15 @@
+const accountId=14432
+let accountEmail="mudgal10harshal@gmail.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+
+//accountId=2
+console.log(accountId);
+accountEmail="gasr@32"
+accountCity="Delhi"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
