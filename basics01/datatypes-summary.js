d9eb264d86e6706c6a8bb389354fca2e0f57cb86
Harshal2826 <mudgal10harshal@gmail.com>
2025-04-06 05:46:43 +0000

    day-1(2)

---
+++ basics01/datatypes-summary.js
@@ -0,0 +1,27 @@
+//data is divided into two parts on the basis of how the data is stored in memory
+/*
+Primitive
+7 types:String,Boolean,Number,Null(empty),undefined,Symbol,Bigint
+*/
+const id=Symbol('1123')
+const id2=Symbol('1123')
+console.log(id===id2);
+//JavaScript is a dynamically typed language.
+/*
+Refrence(non-primitive)
+Array,objects,functions
+*/
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
