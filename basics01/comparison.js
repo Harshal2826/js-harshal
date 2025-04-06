cede67e0ea48c2f7e4dd57b80b88c89e0a52ca57
Harshal2826 <mudgal10harshal@gmail.com>
2025-04-06 05:21:40 +0000

    day-1(2)

---
+++ basics01/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+/*
+NOTE:equality check and comparison operators work differently 
+comparisons convert Null to a number and treat it as zero 
+but we mostly avoid these type of comaprisons
+*/
+// === //checks strictly
+
+console.log("2" === 2);
